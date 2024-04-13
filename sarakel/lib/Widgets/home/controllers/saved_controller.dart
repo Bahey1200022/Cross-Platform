7f6b17d0b6723c261403e8d704a4882a320321e0
Bahey <bahy2002@gmail.com>
2024-04-13 03:10:33 +0200

    heloo

--- sarakel/lib/Widgets/home/controllers/saved_controller.dart
+++ sarakel/lib/Widgets/home/controllers/saved_controller.dart
@@ -1,32 +1,30 @@
-import 'package:flutter/material.dart';
+import 'package:sarakel/constants.dart';
 import 'package:sarakel/models/post.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class SavedController {
-  final String baseUrl = 'http://localhost:3000';
-
   Future<List<Post>> fetchSavedPosts() async {
     try {
-      var response = await http.get(Uri.parse('$baseUrl/savedPosts'));
+      var response = await http.get(Uri.parse('$BASE_URL/subreddit/getBest'));
 
       if (response.statusCode == 200) {
         List<dynamic> jsonData = json.decode(response.body);
 
         List<Post> savedPosts = jsonData.map((p) {
           return Post(
-            communityName: p['communityName'],
-            duration: p['duration'],
-            upVotes: p['upVotes'] ?? 0,
-            downVotes: p['downVotes'] ?? 0,
-            comments: p['comments'],
-            shares: p['shares'],
-            content: p['content'],
-            imagePath: p['imagePath'],
-            communityId: p['communityId'],
-            title: p['title'], 
-            id: 'id',
-          );
+              communityName: p['communityName'],
+              id: p['postId'],
+              duration: p['duration'],
+              upVotes: p['upvotes'] ?? 0, // Provide a default value if null
+              downVotes: p['downvotes'] ?? 0, // Provide a default value if null
+              comments: p['numComments'],
+              shares: p['numComments'],
+              content: p['content'],
+              communityId: p['communityId'],
+              title: p['title'],
+              username: p['userId'],
+              views: p['numViews']);
         }).toList();
 
         return savedPosts;
