65d3050595ca20b253f1c7bc2dcbfd9724bd7af0
RabieAshraf <97186255+RabieAshraf@users.noreply.github.com>
2024-04-14 12:12:55 +0200

    forgotpassword backend

--- sarakel/lib/Widgets/entry/forgot_password.dart
+++ sarakel/lib/Widgets/entry/forgot_password.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:sarakel/constants.dart';
 
 class ForgotPasswordPage extends StatelessWidget {
   final TextEditingController emailOrUsernameController =
@@ -56,8 +59,46 @@ class ForgotPasswordPage extends StatelessWidget {
                     ),
                     SizedBox(height: 16.0),
                     ElevatedButton(
-                      onPressed: () {
-                        // Handle Reset Password button press
+                      onPressed: () async {
+                        String emailOrUsername =
+                            emailOrUsernameController.text.trim();
+                        var data = {'emailOrUsername': emailOrUsername};
+                        var url = Uri.parse('$BASE_URL/login/forget_password');
+                        try {
+                          var response = await http.post(
+                            url,
+                            body: json.encode(data),
+                            headers: {'Content-Type': 'application/json'},
+                          );
+                          if (response.statusCode == 200) {
+                            // Password reset email sent successfully
+                            // Optionally, show a success message to the user
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                    'Password reset email sent successfully'),
+                              ),
+                            );
+                          } else {
+                            // Failed to send password reset email, handle the error
+                            print(
+                                'Failed to send password reset email: ${response.statusCode}');
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content:
+                                    Text('Failed to send password reset email'),
+                              ),
+                            );
+                          }
+                        } catch (e) {
+                          // Handle network errors
+                          print('Error: $e');
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Network error occurred'),
+                            ),
+                          );
+                        }
                       },
                       child: Text(
                         'Reset Password',
