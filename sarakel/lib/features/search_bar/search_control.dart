60ea080ce6c8dc08c5f04203f4a32bf6c1e033a7
Bahey <bahy2002@gmail.com>
2024-04-16 10:41:37 +0200

    search added temporarly

--- sarakel/lib/features/search_bar/search_control.dart
+++ sarakel/lib/features/search_bar/search_control.dart
@@ -1,4 +1,12 @@
-class SearchSerkel {
-  List<String> suggestions;
-  SearchSerkel({this.suggestions = const []});
+import 'package:sarakel/Widgets/drawers/community_drawer/list_controller.dart';
+import 'package:sarakel/models/community.dart';
+
+Future<List<Community>> fetchSuggestions(String query) async {
+  List<Community> suggestions = await loadCircles();
+  suggestions = suggestions
+      .where(
+          (element) => element.name.toLowerCase().contains(query.toLowerCase()))
+      .toList();
+
+  return suggestions;
 }
--- sarakel/lib/features/search_bar/search_screen.dart
+++ sarakel/lib/features/search_bar/search_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:sarakel/Widgets/home/controllers/home_screen_controller.dart';
+import 'package:sarakel/Widgets/profiles/communityprofile_page.dart';
+import 'package:sarakel/models/community.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sarakel/Widgets/home/homescreen.dart';
 import 'package:sarakel/features/search_bar/search_control.dart';
 
 class sarakelSearch extends SearchDelegate {
-  SearchSerkel searchSerkel = SearchSerkel();
-
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
@@ -39,44 +39,51 @@ class sarakelSearch extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    final List<String> suggestionList = query.isEmpty
-        ? searchSerkel.suggestions
-        : searchSerkel.suggestions.where((p) => p.startsWith(query)).toList();
-
-    return ListView.builder(
-      itemBuilder: (context, index) => ListTile(
-        onTap: () async {
-          SharedPreferences prefs = await SharedPreferences.getInstance();
-          String? token = prefs.getString('token');
-          query = suggestionList[index];
-          // Navigate to the desired page
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => SarakelHomeScreen(
-                homescreenController: HomescreenController(token: token!),
+    return FutureBuilder<List<Community>>(
+      future: fetchSuggestions(query), // Pass the query to the API call
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final suggestionList = snapshot.data!;
+          return ListView.builder(
+            itemBuilder: (context, index) => ListTile(
+              onTap: () async {
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                String? token = prefs.getString('token');
+                query = suggestionList[index].name;
+                // Navigate to the desired page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CommunityProfilePage(
+                        community: suggestionList[index], token: token!),
+                  ),
+                );
+              },
+              leading: Icon(Icons.history),
+              title: RichText(
+                text: TextSpan(
+                  text: suggestionList[index].name.substring(0, query.length),
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: suggestionList[index].name.substring(query.length),
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
               ),
             ),
+            itemCount: suggestionList.length,
           );
-        },
-        leading: Icon(Icons.history),
-        title: RichText(
-          text: TextSpan(
-            text: suggestionList[index].substring(0, query.length),
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-            ),
-            children: [
-              TextSpan(
-                text: suggestionList[index].substring(query.length),
-                style: TextStyle(color: Colors.grey),
-              ),
-            ],
-          ),
-        ),
-      ),
-      itemCount: suggestionList.length,
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
     );
   }
 }
