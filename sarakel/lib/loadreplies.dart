2f73fe4f3b49d67eaf3981bc78e5cdb0c44d4130
Ahmed Hafez <ahmed.hafez307@gmail.com>
2024-05-03 20:04:47 +0300

    comments and replies

--- sarakel/lib/loadreplies.dart
+++ sarakel/lib/loadreplies.dart
@@ -1,30 +1,47 @@
 import 'package:http/http.dart' as http;
 import 'package:sarakel/constants.dart';
-import 'package:sarakel/models/reply.dart'; // Make sure you have a reply model
+import 'package:sarakel/models/comment.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
-Future<List<Reply>> fetchReplies(String postID) async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  var token = prefs.getString('token');
-  if (token == null) {
-    throw Exception('No token found');
-  }
+Future<List<Comment>> fetchReplies(String postId, String commentId) async {
+  try {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
 
-  var response = await http.post(
-    Uri.parse('$BASE_URL/api/get_post_replies'),
-    headers: {
-      'Authorization': 'Bearer $token',
-      'Content-Type': 'application/json',
-    },
-    body: jsonEncode({'postID': postID}),
-  );
+    if (token == null) {
+      throw Exception('Token not found');
+    }
 
-  if (response.statusCode == 200) {
-    List<dynamic> data = jsonDecode(
-        response.body)['message']; // Adjust the key based on actual response
-    return data.map((json) => Reply.fromJson(json)).toList();
-  } else {
-    throw Exception('Failed to load Replies: ${response.body}');
+    Uri url = Uri.parse('$BASE_URL/api/get_comment_replies');
+
+    http.Response response = await http.post(
+      url,
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'postId': postId,
+        'commentId': commentId,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> responseData = json.decode(response.body);
+      List<dynamic> commentList = responseData['message'] ?? [];
+      List<Comment> replies = commentList.map((json) => Comment.fromJson(json)).toList();
+      return replies;
+    } else if (response.statusCode == 400 && response.body.contains("Comment not found")) {
+      print('Failed to load replies: Comment not found');
+      return [];
+    } else {
+      print('Failed to load replies: ${response.statusCode}');
+      print('Response body: ${response.body}');
+      throw Exception('Failed to load replies');
+    }
+  } catch (e) {
+    print('Failed to load replies: $e');
+    throw Exception('Failed to load replies');
   }
 }
