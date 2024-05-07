23ba6e744e011ac6a96cddbfb01458cd1e2e25ec
Ahmed Hafez <ahmed.hafez307@gmail.com>
2024-05-07 18:10:17 +0300

    Chatting folder

--- sarakel/lib/Widgets/chatting/chat_page.dart
+++ sarakel/lib/Widgets/chatting/chat_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:sarakel/Widgets/chatting/conversations.dart';
