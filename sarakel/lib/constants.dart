7b49c980a72ce86621caccb8040550ffc7a31542
Bahey <bahy2002@gmail.com>
2024-05-07 19:36:36 +0300

    .

--- sarakel/lib/constants.dart
+++ sarakel/lib/constants.dart
@@ -5,9 +5,8 @@ const ip = '57.151.116.81'; //don't change this ever
 //ged3an ma7adesh yeghayar da 3ayez teghayar fel beta3 e3mel comment lel base url el oula
 //w 7ot el ip address beta3ak fel base url el tanya
 
-//const BASE_URL = 'http://$ip:5000';
+const BASE_URL = 'http://$ip:5000';
 
 //const BASE_URL = 'http://192.168.100.17:5000'; //for  running locally
 
 //const BASE_URL = 'http://192.168.1.11:5000';   //for  running locally
-const BASE_URL = 'http://192.168.1.4:5000';   //for  running locally
